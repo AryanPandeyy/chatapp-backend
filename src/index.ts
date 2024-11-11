9e86db01d4109036f1177879bebe8dd2f8df64eb
Aryan Pandey <margiaryan@gmail.com>
2024-11-11 21:23:21 +0530

    feat: mongodb added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import { ClientToServerEvents, InterServerEvents, ServerToClientEvents, SocketData } from "./types/socket";
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(cors({origin: true}));
@@ -10,6 +11,7 @@ const httpServer = createServer(app);
 const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {cors: {
     origin: "http://localhost:5173"
 }});
+const connectionString = process.env.URI || "";
 
 app.get("/", (res: Response) => {
     res.send("hi");
@@ -22,4 +24,6 @@ io.on("connection", (socket) => {
     });
 });
 
+mongoose.connect(connectionString).then(result => {
 httpServer.listen(3000);
+}).catch(err => console.log("ERROR: ", err));
